f9651afda2124f5322947cab5d2ea58a42866852
Austin Davis <insdavm@gmail.com>
2017-03-09 18:37:05 -0500

    Initial commit

---
+++ src/rainbow-pixels.cpp
@@ -0,0 +1,71 @@
+/**
+ * rainbow-pixels
+ * 
+ * ...cycles a single WS2812B RGB LED (like the Adafruit NeoPixels)
+ * through the visible color spectrum.
+ *
+ * @author Austin <github.com/insdavm>
+ * @date 2017.02.18
+ */
+
+#include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
+
+#define   PIN   3                                         /* the pin that our WS2812B LED is on */
+
+Adafruit_NeoPixel p = Adafruit_NeoPixel(1, PIN);          /* create a NeoPixel object consisting of one pixel */
+uint8_t rgb[3];                                           /* declare an array of 8-bit integers to hold our color values */
+
+
+void setup()
+{
+  p.begin();
+  p.show();
+  p.setBrightness(255);
+}
+
+void loop()
+{
+
+  /* 
+   * rgb[0] is synonymous with red,
+   * rgb[1] "                " green,
+   * rbg[2] "                " blue. 
+   */
+  rgb[0] = 255;
+  rgb[1] = 0;
+  rgb[2] = 0;
+
+  /* 
+   * loop through each combination of 
+   *     RED   /  GREEN    (dC = 0, iC = 1)
+   *     GREEN /  BLUE     (dC = 1, iC = 2)
+   *     BLUE  /  RED      (dC = 2, iC = 0)
+   */
+  for ( int dC = 0; dC < 3; dC++ ) {
+
+    /*
+     * make the incrementing color (iC) be one more than
+     * the decrementing color (dC) or, if the iC is 2 (blue), 
+     * then the dC should wrap around to 0 (red).
+     */
+    int iC = dC == 2 ? 0 : dC + 1;
+
+    /*
+     * now that our incrementing and decrementing colors have
+     * been identified, increment and decrement respectively 
+     * each one by a value of 1 for 255 times (the range of 
+     * possible values for each color)
+     */
+    for ( int i = 0; i < 255; i++ ) {
+
+      rgb[dC]--;
+      rgb[iC]++;
+
+      p.setPixelColor(0, rgb[0], rgb[1], rgb[2]);
+      p.show();
+      delay(30);
+      
+    } 
+  }
+}
